9c9b17fcaae83e98b44b9e1908b31ad084db9922
Jakob Guttmann <alu0101685854@ULL.LOCAL>
2023-10-02 14:35:43 +0100

    Exercise 4, edited

--- Ex4_SpherePosition.cs
+++ Ex4_SpherePosition.cs
@@ -7,13 +7,12 @@ public class Ex4_SpherePosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        UnityEngine.Vector3 position = transform.position;
+        Debug.Log("Sphere position: " + position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        UnityEngine.Vector3 position = transform.position;
-        Debug.Log("Sphere position: " + position);
     }
 }
